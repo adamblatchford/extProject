c287cbe26c79c992c68a6243d0a09e81189a8f44
adamblatchford <phil.baxter@equiteq.com>
2023-07-05 11:43:04 +0100

    Update example-card.js

--- src/app/app.functions/example-card.js
+++ src/app/app.functions/example-card.js
@@ -1,6 +1,6 @@
 exports.main = async (context = {}, sendResponse) => {
 	const {
-		propertiesToSend: { name, numberofemployees, growth1yr_employees__linkedin_,is_equiteq_sell_side_in_market_, sip___is_cancelled_on_hold, sip___is_coming_to_market, sip___is_coming_to_market__sales_opp_, sip___is_considering_a_transaction, hs_lead_status, lifecyclestage,equiteq_region,type_of_company },
+		propertiesToSend: { name, numberofemployees, growth1yr_employees__linkedin_,is_equiteq_sell_side_in_market_, sip___is_cancelled_on_hold, sip___is_coming_to_market, sip___is_coming_to_market__sales_opp_, sip___is_considering_a_transaction, is_deal_in_market_, is_equiteq_buy_side_target_in_market_, live_recent_transaction_, hs_lead_status, lifecyclestage,equiteq_region,type_of_company },
 	} = context;
 	
 	try {
@@ -10,11 +10,57 @@ exports.main = async (context = {}, sendResponse) => {
 		const Alert_sip___is_coming_to_market = true;
 		const Alert_sip___is_coming_to_market__sales_opp_ = true;
 		const Alert_sip___is_considering_a_transaction = true;
-		
+
+		const Alert_is_deal_in_market_ = true;
+		const Alert_is_equiteq_buy_side_target_in_market_ = true;
+		const Alert_live_recent_transaction_ = true;
+			
 		sendResponse({
 			
 			title: 'This card retrieves key contact details.',
 			sections: [
+				...(Alert_live_recent_transaction_
+					    ? [
+						    {
+							    type: 'alert',
+							    title: 'Deal Alert!',
+							    variant: 'error',
+							    body: {
+								    type: 'text',
+								    text: 'Company has recently transacted',
+							    },
+						    },
+					    ]
+					    : []),
+				
+				...(Alert_is_deal_in_market_
+					    ? [
+						    {
+							    type: 'alert',
+							    title: 'Deal in Market',
+							    variant: 'warning',
+							    body: {
+								    type: 'text',
+								    text: 'Company in process with a 3rd party advisor',
+							    },
+						    },
+					    ]
+					    : []),
+
+				...(Alert_is_equiteq_buy_side_target_in_market_
+					    ? [
+						    {
+							    type: 'alert',
+							    title: 'Buy side target',
+							    variant: 'warning',
+							    body: {
+								    type: 'text',
+								    text: 'Company is a target on a Equiteq deal',
+							    },
+						    },
+					    ]
+					    : []),
+
 				...(Alert_is_equiteq_sell_side_in_market_
 					    ? [
 						    {
