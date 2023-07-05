c2248cd113f6a8c817996b20ea235108b417a791
adamblatchford <phil.baxter@equiteq.com>
2023-07-05 10:36:58 +0100

    Update example-card.js

--- src/app/app.functions/example-card.js
+++ src/app/app.functions/example-card.js
@@ -10,58 +10,69 @@ exports.main = async (context = {}, sendResponse) => {
 		sendResponse({
 			
 			title: 'This card retrieves key contact details.',
-			sections: [
-				
-				 "type": "tile",
-				 "body": [
-				{
-					type: 'statistics',
-					items: [
-						  {
-							  label: 'Number Employees',
-							  number: numberofemployees,
-							  description: {
-								  type: 'trend',
-								  value: growth1yr_employees__linkedin_,
-								  direction: direction,
-							  }
-						  },
-						  
-						  {
-							  label: 'Number Employees',
-							  number: numberofemployees,
-							  description: {
-								  type: 'trend',
-								  value: growth1yr_employees__linkedin_,
-								  direction: direction,
-							  }
-						  }
-					  	],
-					},
-					
-					 {
-						 "type": "divider",
-						 "distance": "small"
-					 },
-					 
-					 {
-						 "type": "text",
-						 "format": "markdown",
-						 "text": "**Engagement**"
-					},
-					 
-					 {
-						 "type": "progressBar",
-						 "variant": "warning",
-						 "valueMax": 100,
-						 "value": 20,
-						 "title": "Bottles",
-						 "valueDescription": "20 out of 100",
-						 "showPercentage": true
-					 },
-				 ],
-				}
-			],
+			  sections: [
+			    {
+			      type: 'tile',
+			      body: [
+			        {
+			          type: 'statistics',
+			          items: [
+			            {
+			              label: 'Number Employees',
+			              number: numberofemployees,
+			              description: {
+			                type: 'trend',
+			                value: growth1yr_employees__linkedin_,
+			                direction: direction,
+			              },
+			            },
+			            {
+			              label: 'Number Employees',
+			              number: numberofemployees,
+			              description: {
+			                type: 'trend',
+			                value: growth1yr_employees__linkedin_,
+			                direction: direction,
+			              },
+			            },
+			          ],
+			        },
+			        // Conditionally include the "alert" section based on a boolean variable
+			        ...(shouldIncludeAlertSection
+			          ? [
+			              {
+			                type: 'alert',
+			                title: 'Equiteq Deal',
+			                variant: 'success',
+			                body: {
+			                  type: 'text',
+			                  text: 'Live Mandate',
+			                },
+			              },
+			            ]
+			          : []),
+			        {
+			          type: 'divider',
+			          distance: 'small',
+			        },
+			        {
+			          type: 'text',
+			          format: 'markdown',
+			          text: '**Engagement**',
+			        },
+			        {
+			          type: 'progressBar',
+			          variant: 'warning',
+			          valueMax: 100,
+			          value: 20,
+			          title: 'Bottles',
+			          valueDescription: '20 out of 100',
+			          showPercentage: true,
+			        },
+			      ],
+			    },
+			  ],
+
 		});
 	} catch (error) {
 		console.error(error);
