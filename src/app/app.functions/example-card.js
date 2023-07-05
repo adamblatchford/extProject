3ff3afb3538896a9a5878734b0f25154d27ab809
adamblatchford <phil.baxter@equiteq.com>
2023-07-05 10:01:55 +0100

    Update example-card.js

--- src/app/app.functions/example-card.js
+++ src/app/app.functions/example-card.js
@@ -1,75 +1,79 @@
 exports.main = async (context = {}, sendResponse) => {
-  const {
-    propertiesToSend: { name, numberofemployees, growth1yr_employees__linkedin_,is_equiteq_sell_side_in_market_ },
-  } = context;
-  try {
-	   const direction = growth1yr_employees__linkedin_ > 0 ? 'increase' : 'decrease';
-
-    sendResponse({
-     
-  title: 'This card retrieves key contact details.',
-  sections: [
-  {
- "type": "tile",
- "body": [
-        {
-          type: 'statistics',
-          items: [
-            {
-			
-              label: 'Number Employees',
-			  number: numberofemployees,
-			  description: {
-				  type: 'trend',
-				  value: growth1yr_employees__linkedin_,
-              direction: direction,
-			  }
-            },
-			
-			{
-			
-			label: 'Number Employees',
-			  number: numberofemployees,
-			  description: {
-				  type: 'trend',
-				  value: growth1yr_employees__linkedin_,
-              direction: direction,
-			  }			
-				
-				
-			}
-			
-			
-			
-          ],
-		  
-        },
-		{"type": "tag",
-		"text": "completed",
-    "variant": "success", },
-	{
-   "type": "divider",
-   "distance": "small"
-   },
-
-	{
-   "type": "text",
-   "format": "markdown",
-   "text": "**Engagement**"
-	},
+	const {
+		propertiesToSend: { name, numberofemployees, growth1yr_employees__linkedin_,is_equiteq_sell_side_in_market_ },
+	} = context;
 	
-	 {
-    "type": "progressBar",
-    "variant": "warning",
-    "valueMax": 100,
-    "value": 20,
-    "title": "Bottles",
-    "valueDescription": "20 out of 100",
-    "showPercentage": true
-  },
-      ],
-  }], });
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const direction = growth1yr_employees__linkedin_ > 0 ? 'increase' : 'decrease';
+		
+		sendResponse({
+			
+			title: 'This card retrieves key contact details.',
+			sections: [
+				{
+				 "type": "tile",
+				 "body": [
+					 
+					 {
+						 type: 'statistics',
+						 items: [
+						  {
+							  label: 'Number Employees',
+							  number: numberofemployees,
+							  description: {
+								  type: 'trend',
+								  value: growth1yr_employees__linkedin_,
+								  direction: direction,
+							  }
+						  },
+						  
+						  {
+							  label: 'Number Employees',
+							  number: numberofemployees,
+							  description: {
+								  type: 'trend',
+								  value: growth1yr_employees__linkedin_,
+								  direction: direction,
+							  }
+						  }
+					  	],
+					},
+					 
+					 {
+						 "type": "alert",
+						 "title": "Equiteq Deal",
+						 "variant": "success",
+						 "body": {
+							 "type": "text",
+							 "text": "Live Mandate"
+						 }
+					 },
+					 
+					 {
+						 "type": "divider",
+						 "distance": "small"
+					 },
+					 
+					 {
+						 "type": "text",
+						 "format": "markdown",
+						 "text": "**Engagement**"
+					},
+					 
+					 {
+						 "type": "progressBar",
+						 "variant": "warning",
+						 "valueMax": 100,
+						 "value": 20,
+						 "title": "Bottles",
+						 "valueDescription": "20 out of 100",
+						 "showPercentage": true
+					 },
+				 ],
+				}
+			],
+		});
+	} catch (error) {
+		console.error(error);
+	}
 };
